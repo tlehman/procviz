2c1c28762b61379ce12d8d9b06c89e1a00171b5d
Tobi Lehman <tobi.lehman@gmail.com>
2012-10-28 13:37:14 -0700

    Add node generator in Ruby

--- arborjs/gen_graph.rb
+++ arborjs/gen_graph.rb
@@ -1,2 +1,13 @@
 # this is the data we will use to construct the arborjs graph
 pid_ppid_comm = `ps axo ppid,pid,comm | grep -v PID`
+
+ppcs = pid_ppid_comm.split("\n")
+
+class Node < Struct.new(:ppid, :pid, :command); end
+nodes = Array.new
+
+ppcs.each do |ppc|
+  /(^(\s*)(?<ppid>\d+))\s+(?<pid>\d+)\s+(?<comm>(.*))$/ =~ ppc
+  nodes << Node.new(ppid, pid, comm)
+end
+
