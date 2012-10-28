557a3a8742ce71c5ec2c1235af47e0e9eb30a287
Tobi Lehman <tobi.lehman@gmail.com>
2012-10-28 13:44:38 -0700

    Rubify it

--- gen_graph.rb
+++ gen_graph.rb
@@ -1,14 +1,24 @@
-# this is the data we will use to construct the arborjs graph
-pid_ppid_comm = `ps axo ppid,pid,comm | grep -v PID`
-
-ppcs = pid_ppid_comm.split("\n")
-
-class Node < Struct.new(:ppid, :pid, :command); end
-nodes = Array.new
-
-ppcs.each do |ppc|
-  /(^(\s*)(?<ppid>\d+))\s+(?<pid>\d+)\s+(?<comm>(.*))$/ =~ ppc
-  nodes << Node.new(ppid, pid, comm)
+def proc_data
+  pid_ppid_comm = `ps axo ppid,pid,comm | grep -v PID`
+  ppcs = pid_ppid_comm.split("\n")
 end
 
+class Node < Struct.new(:ppid, :pid, :command); end
 
+class Graph
+  attr_reader :nodes
+  
+  def initialize(node_list)
+    @nodes = []
+    node_list.each do |ppc|
+      /(^(\s*)(?<ppid>\d+))\s+(?<pid>\d+)\s+(?<comm>(.*))$/ =~ ppc
+      @nodes << Node.new(ppid, pid, comm)
+    end
+  end
+  
+  def to_arborjs
+  end
+
+  def to_graphviz
+  end
+end
